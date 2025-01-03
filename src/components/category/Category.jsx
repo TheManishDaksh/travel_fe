8dd193e4a89d326fc16a0aad6fb9d0d6f3a9524c
TheManishDaksh <140195332+TheManishDaksh@users.noreply.github.com>
2025-01-03 23:22:01 +0530

    filter added

--- src/components/category/Category.jsx
+++ src/components/category/Category.jsx
@@ -1,16 +1,21 @@
 import axios from "axios"
 import { useEffect, useState } from "react"
+import { useCategory } from "../../context/categoryContext"
 
 function Category(){
     
     const [categories, setCategories] = useState([])
     const [categoryToShow, setCategoryToShow] = useState(0)
+    const { hotelCategory, setHotelCategory} = useCategory()
 
     function handleCategoryRight (){
         setCategoryToShow((prev) => prev+10)
     }
     function handleCategoryLeft(){
         setCategoryToShow((prev) => prev - 10)
+    }
+    function handleHotelCategory({category}){
+        setHotelCategory(category)
     }
 
     useEffect(()=>{
@@ -25,16 +30,21 @@ function Category(){
     })()
     },[categoryToShow])
     return(
-       <section className="p-3 px-9 font-normal flex gap-10 "> 
+       <section className="p-3 px-9 font-normal flex gap-10"> 
+
         <button onClick={handleCategoryLeft}
         ><span class="material-symbols-outlined">chevron_left    
         </span></button>
-        {categories.map(({category,_id})=><span className="cursor-pointer"
+
+        {categories.map(({category,_id})=>  
+        <span className={`cursor-pointer ${category === hotelCategory ? "border-b-2 border-solid border-black cursor-pointer" : "" }`}
+        onClick={() => handleHotelCategory({category})}
         key={_id}>{category}</span>)} 
+
         <button onClick={handleCategoryRight}
         ><span class="material-symbols-outlined">chevron_right   
         </span></button> 
-       </section>
+       </section>   
     )
 }
 export default Category
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -3,25 +3,27 @@ import HotelCard from '../components/hotelCard/HotelCard'
 import Navbar from '../components/navbar/Navbar'
 import axios from "axios"
 import Category from '../components/category/category'
+import { useCategory } from '../context/categoryContext'
 
 function HomePage() {
 
-    const [hotels, setHotels] = useState([])
+    const [hotels, setHotels] = useState([]);
+    const {hotelCategory} = useCategory();
     useEffect(()=>{
         (async ()=>{
             try{
-            const {data} = await axios.get('http://localhost:3000/api/hotels')
+            const {data} = await axios.get(`http://localhost:3000/api/hotels?category=${hotelCategory}`)
             setHotels(data)
             }catch(error){
                 console.log(error);
             }
         })()
-    },[])
+    },[hotelCategory])
   return (
     <div> 
         <Navbar className="sticky"/>
         <Category/>
-        <main className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-7 p-5 pl-9 overflow-y-auto h-screen '>
+        <main className='grid grid-cols-4 gap-7 p-5 pl-9 overflow-y-auto  '>
         {
             hotels.map((hotel)=> <HotelCard key={hotel._id}
             name={hotel.name}
